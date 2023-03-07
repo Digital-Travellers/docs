4901febc7559428008b1356a5d7f7ba64aa4e5d0
Miquel de Domingo <miquelddg@gmail.com>
2023-03-07 16:47:23 +0100

    fix: simplify home page

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -1,19 +1,14 @@
-import useDocusaurusContext from '@docusaurus/useDocusaurusContext';
 import { Content } from '@site/src/components';
 import Layout from '@theme/Layout';
 
 import React from 'react';
 
-export const Home = (): JSX.Element => {
-  const { siteConfig } = useDocusaurusContext();
-
-  return (
-    <Layout title={`Home - ${siteConfig.title}`} description="">
-      <main className="px-8 py-8">
-        <Content />
-      </main>
-    </Layout>
-  );
-};
+export const Home = (): JSX.Element => (
+  <Layout title="Home" description="">
+    <main className="px-8 py-8">
+      <Content />
+    </main>
+  </Layout>
+);
 
 export default Home;

215eae3d2270ac3eae7d9669c524880bfa63507d
Miquel de Domingo <miquelddg@gmail.com>
2023-03-10 23:13:58 +0100

    feat: update repositories in home page

--- src/components/shared/headers/headers.tsx
+++ src/components/shared/headers/headers.tsx
@@ -5,6 +5,7 @@ import React from 'react';
 type HeaderProps = {
   id?: string;
   content: string;
+  asCode?: boolean;
 };
 
 const useHeaderColor = () => {
@@ -13,46 +14,71 @@ const useHeaderColor = () => {
   return colorMode === 'dark' ? 'text-neutral-100' : 'text-neutral-700';
 };
 
-export const H1 = ({ id, content }: HeaderProps): JSX.Element => {
+export const H1 = ({ asCode, id, content }: HeaderProps): JSX.Element => {
   const color = useHeaderColor();
   return (
-    <h1 id={id} className={`mb-4 text-4xl font-bold ${color}`}>
+    <h1
+      id={id}
+      className={`mb-4 text-4xl font-bold ${color}${
+        asCode ? ' font-mono' : ''
+      }`}
+    >
       {content}
     </h1>
   );
 };
 
-export const H2 = ({ id, content }: HeaderProps): JSX.Element => {
+export const H2 = ({ asCode, id, content }: HeaderProps): JSX.Element => {
   const color = useHeaderColor();
   return (
-    <h2 id={id} className={`mb-4 text-3xl font-bold ${color}`}>
+    <h2
+      id={id}
+      className={`mb-4 text-3xl font-bold ${color}${
+        asCode ? ' font-mono' : ''
+      }`}
+    >
       {content}
     </h2>
   );
 };
 
-export const H3 = ({ id, content }: HeaderProps): JSX.Element => {
+export const H3 = ({ asCode, id, content }: HeaderProps): JSX.Element => {
   const color = useHeaderColor();
   return (
-    <h3 id={id} className={`mb-4 text-2xl font-semibold ${color}`}>
+    <h3
+      id={id}
+      className={`mb-4 text-2xl font-semibold ${color}${
+        asCode ? ' font-mono' : ''
+      }`}
+    >
       {content}
     </h3>
   );
 };
 
-export const H4 = ({ id, content }: HeaderProps): JSX.Element => {
+export const H4 = ({ asCode, id, content }: HeaderProps): JSX.Element => {
   const color = useHeaderColor();
   return (
-    <h4 id={id} className={`mb-2 text-xl font-semibold ${color}`}>
+    <h4
+      id={id}
+      className={`mb-2 text-xl font-semibold ${color}${
+        asCode ? ' font-mono' : ''
+      }`}
+    >
       {content}
     </h4>
   );
 };
 
-export const H5 = ({ id, content }: HeaderProps): JSX.Element => {
+export const H5 = ({ asCode, id, content }: HeaderProps): JSX.Element => {
   const color = useHeaderColor();
   return (
-    <h5 id={id} className={`mb-2 text-lg font-semibold ${color}`}>
+    <h5
+      id={id}
+      className={`mb-2 text-lg font-semibold ${color}${
+        asCode ? ' font-mono' : ''
+      }`}
+    >
       {content}
     </h5>
   );
